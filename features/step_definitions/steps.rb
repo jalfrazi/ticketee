abd96971892febab76bf2a27e16a1b11e7bdb6ba
Frank McArdle <frankpmcardle@gmail.com>
2013-06-11 15:58:46 +0100

    Implemented features for displaying a list of relevant tickets for projects and viewing particular tickets

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -14,7 +14,7 @@ When(/^I press "(.*?)"$/) do |arg1|
   click_button(arg1)
 end
 
-Then(/^I should see "(.*?)"$/) do |arg1|
+Then(/^I should see "([^"]*)"$/) do |arg1|
   #page.has_text?(:visible, arg1)
   page.should have_content(arg1)
 end
@@ -28,9 +28,19 @@ Then(/^the title should be "(.*?)"$/) do |arg1|
 end
 
 Given(/^there is a project called "(.*?)"$/) do |arg1|
-  FactoryGirl.create(:project, name: arg1)
+  @project = FactoryGirl.create(:project, name: arg1)
 end
 
 Then(/^I should not see "(.*?)"$/) do |arg1|
   page.should_not have_content(arg1)
+end
+
+Given(/^that project has a ticket:$/) do |table|
+  table.hashes.each do |attributes|
+    @project.tickets.create!(attributes)
+  end
+end
+
+Then(/^I should see "(.*?)" in the selector "(.*?)"$/) do |arg1, arg2|
+  page.should have_selector arg2, text: arg1
 end
